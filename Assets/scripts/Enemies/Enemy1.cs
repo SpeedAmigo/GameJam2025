cc0f5a0ca566eb80d727cab6bc978bad04a902b7
SpeedAmigo <Kxipi7@gmail.com>
2025-02-08 21:09:41 +0100

    fix

--- Assets/scripts/Enemies/Enemy1.cs
+++ Assets/scripts/Enemies/Enemy1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -21,6 +22,8 @@ public class Enemy1 : MonoBehaviour, IInteractable
     private int _startActionInterval;
     private bool _isJammed;
     
+    private Coroutine _actionCoroutine;
+    private bool _coroutineRunning;
     
     [SerializeField] private int _satisfactionIncrease;
 
@@ -73,12 +76,17 @@ public class Enemy1 : MonoBehaviour, IInteractable
             yield return new WaitForSeconds(_actionInterval);
         
             _actionInterval = Random.Range(actionIntervalRange.x, actionIntervalRange.y + 1);
-            StartCoroutine(PerformAction());
+
+            if (_coroutineRunning == false)
+            {
+                StartCoroutine(PerformAction());
+            }
         }
     }
     
     private IEnumerator PerformAction()
     {
+        _coroutineRunning = true;
         List<Sprite> selectedList;
         bool isWrongAction = Random.Range(0f, 100f) < enemySO.wrongActionChance;
         wrongAction = false;
@@ -101,6 +109,7 @@ public class Enemy1 : MonoBehaviour, IInteractable
             {
                 wrongAction = true;
                 yield return new WaitForSeconds(wrongActionDuration);
+                _coroutineRunning = false;
             }
             else
             {
@@ -115,9 +124,15 @@ public class Enemy1 : MonoBehaviour, IInteractable
                 }
             }
         }
+
+        if (!_isJammed)
+        {
+            _spriteRenderer.sprite = enemySO.npcDefaultSprite;
+        }
         
-        _spriteRenderer.sprite = enemySO.npcDefaultSprite;
+        
         wrongAction = false;
+        _coroutineRunning = false;
     }
     private void Start()
     {
