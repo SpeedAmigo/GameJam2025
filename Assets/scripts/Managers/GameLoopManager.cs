075a2c06241699b55a69b072bd142f7c1b458aef
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-02-08 02:59:24 +0100

    add manager file

---
+++ Assets/scripts/Managers/GameLoopManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoopManager
+{
+    private static GameLoopManager _instance;
+
+    public static GameLoopManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+                _instance = new GameLoopManager();
+            
+            return _instance;
+        }
+    }
+
+    private int _satisfaction = 50;
+
+    public int Satisfaction
+    {
+        get { return _satisfaction; }
+        set { _satisfaction = value; }
+    }
+}
