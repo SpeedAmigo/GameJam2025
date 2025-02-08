d06c644a507562baa107e5d1fcdedebd627df0cd
SpeedAmigo <Kxipi7@gmail.com>
2025-02-08 17:57:21 +0100

    customers sprite actions

--- Assets/scripts/ScriptableObjects/Enemy.cs
+++ Assets/scripts/ScriptableObjects/Enemy.cs
@@ -5,41 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/Enemies", order = 1)]
 public class Enemy : ScriptableObject
 {
-    public GameObject prefab;
-    
-    public Vector2Int actionIntervalRange;
-    public Vector2Int startActionIntervalRange;
-    
-    private int _actionInterval;
-    private int _startActionInterval;
-    
-    public int numberOfAlerts;
-    
-    private void OnEnable()
-    {
-        _actionInterval = Random.Range(actionIntervalRange.x, actionIntervalRange.y + 1);
-        _startActionInterval = Random.Range(startActionIntervalRange.x, startActionIntervalRange.y + 1);
-        
-        Debug.Log("Start Interval" + _startActionInterval);
-        Debug.Log("Event Interval" + _actionInterval);
-    }
-    
-    public IEnumerator InteractionsStarter(MonoBehaviour owner)
-    {
-        yield return new WaitForSeconds(_startActionInterval);
-        Debug.Log("Start being problematic");
+    public Sprite npcDefaultSprite;
+    public List<Sprite> npcNeutralActionSprites;
+    public List<Sprite> npcWrongActionSprites;
 
-        owner.StartCoroutine(CustomerActions());
-    }
-
-    private IEnumerator CustomerActions()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(_actionInterval);
-        
-            _actionInterval = Random.Range(actionIntervalRange.x, actionIntervalRange.y + 1);
-            Debug.Log("new event Interval:" + _actionInterval);
-        }
-    }
+    [Range(0f, 100f)] public float wrongActionChance; 
 }
