dd85beac8cf313bcf8597e0e88af26c3d4d39ddc
SpeedAmigo <Kxipi7@gmail.com>
2025-02-08 02:13:18 +0100

    PlayerWalking

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -4,6 +4,8 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D _body;
     private Vector2 _movement;
+    [SerializeField] private Vector2 _lastMove;
+    private Animator _animator;
     
     [Range(1, 5)]
     [SerializeField] private float _speed;
@@ -14,12 +16,24 @@ public class PlayerScript : MonoBehaviour
     {
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
+
+        if (_movement != Vector2.zero)
+        {
+            _lastMove = _movement;
+        }
+        
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
+        _animator.SetFloat("LastHorizontal", _lastMove.x);
+        _animator.SetFloat("LastVertical", _lastMove.y);
     }
     
     // Start is called before the first frame update
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
