4a0f59f553a75de6acaab0af83b34206bef11e24
SpeedAmigo <Kxipi7@gmail.com>
2025-02-09 10:01:47 +0100

    name set fix

--- Assets/scoreInputScript.cs
+++ Assets/scoreInputScript.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class scoreInputScript : MonoBehaviour
 {
-    [SerializeField] private InputField inputField;
-
+    [SerializeField] private TMP_InputField inputField;
+    
     public void LoadScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void SetName()
+    {
+        GameLoopManager.Instance.PlayerName = inputField.text;
+        
+        Debug.Log(GameLoopManager.Instance.PlayerName);
     }
 }
